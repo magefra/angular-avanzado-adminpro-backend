b612b07b4318df7c3d0a4631e429352fa7040b01
magefra9 <magefra9@hotmail.com>
2021-05-25 18:32:42 -0500

    Agregando  promise.All busqueda

--- controllers/busqueda.js
+++ controllers/busqueda.js
@@ -1,17 +1,32 @@
 const { response } = require("express")
 
+const Usuario = require('../models/usuarios');
+const Medico = require('../models/medico');
+const Hospital = require('../models/hospital');
+
 
 const getBusqueda = async(req, res = response) =>
 {
 
     const busqueda = req.params.busqueda;
+    const regex = new RegExp(busqueda, "i");
+
+
+    const [usuario, medico, hospital] = await Promise.all([
+         Usuario.find({nombre: regex}),
+         Medico.find({nombre: regex}),
+         Hospital.find({nombre: regex})
+
+    ]);
 
 
     console.log(busqueda);
 
         res.status(200).json({
             ok: true,
-            msg: 'getBusqueda'
+            usuarios: usuario,
+            medicos: medico,
+            hospitales: hospital
         });       
 }
 
